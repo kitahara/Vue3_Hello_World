9ff3ae45834fedf0972678ffb659b1999e976fc8
kitahara <rxrjy281@gmail.com>
2022-12-09 16:42:50 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <h1>aaabbbccc</h1>
+  <HelloWorld msg="Hello Vue 3 in CodeSandbox!aaabbbcccddd" />
+</template>
+
+<script>
+import HelloWorldVue from "./components/HelloWorld.vue";
+export default {
+  name: "App",
+  components: {
+    HelloWorld: HelloWorldVue,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
